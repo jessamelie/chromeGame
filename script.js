a76e44033b8fd3bbe87973e13d71a1b53e0bc8f5
jessamelie <jessamelie.sola@gmail.com>
2024-02-21 15:48:08 +0100

    MVP done

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
 let character = document.getElementById("character");
 let block = document.getElementById("block");
 
+//Jump Action Function
 let jump = () => {
   if (character.classList != "animate") {
     character.classList.add("animate");
@@ -10,13 +12,16 @@ let jump = () => {
   }, 500);
 };
 
+// Hit Detection Function
 let hitDetection = setInterval(() => {
+  //position of elements in pixels
   let characterTop = parseInt(
     window.getComputedStyle(character).getPropertyValue("top")
   );
   let blockLeft = parseInt(
     window.getComputedStyle(block).getPropertyValue("left")
   );
+  //collision zone detection 
   if (blockLeft<20 && blockLeft>0 && characterTop>=130) {
     block.style.animation = "none";
     block.style.display = "none";
