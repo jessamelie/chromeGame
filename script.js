915cce917f3165d40f4946de5d60525bac0c6ba0
jessamelie <jessamelie.sola@gmail.com>
2024-02-21 15:20:38 +0100

    hit detection function

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ let jump = () => {
     character.classList.remove("animate");
   }, 500);
 };
+
+let hitDetection = setInterval(() => {
+  let characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  let blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  if (blockLeft<20 && blockLeft>0 && characterTop>=130) {
+    block.style.animation = "none";
+    block.style.display = "none";
+    alert("Too bad! You lose")
+  }
+}, 10);
